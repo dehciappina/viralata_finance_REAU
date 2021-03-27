b7eb24de5ab9c526bd9ff3aedf1e2f30a45fdccd
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2021-03-27 12:02:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,37 @@ window.onscroll = function() {
         header.style.transform = 'translateY(-100%)';
     }
 }
+
+let telegramWindowShowing = false;
+
+const telegramBts = document.querySelectorAll('.telegramtrigger')
+const telegramWindow = document.querySelector('#telegram_window')
+const exitWindow = document.querySelector('#exit_bg')
+
+for(i=0; i < telegramBts.length; i++) {
+
+    telegramBts[i].addEventListener('click', function() {
+        if(telegramWindowShowing == false) {
+            telegramWindowShowing = true;
+
+            exitWindow.style.opacity = 1;
+            exitWindow.style.visibility = 'visible';
+
+            telegramWindow.style.opacity = 1;
+            telegramWindow.style.visibility = 'visible';
+            telegramWindow.style.transform = "translateY(-50%)";
+        }
+    })
+}
+
+
+exitWindow.addEventListener('click', function() {
+    telegramWindowShowing = false;
+
+    exitWindow.style.opacity = 0;
+    exitWindow.style.visibility = 'hidden';
+
+    telegramWindow.style.opacity = 0;
+    telegramWindow.style.visibility = 'hidden';
+    telegramWindow.style.transform = "translateY(-30%)";
+})
